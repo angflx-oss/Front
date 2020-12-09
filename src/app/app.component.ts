c0c9c2e6b77cd1ade0cd9d5a1bfc353c79fed288
angflx-oss <angelafelix98@gmail.com>
2020-12-08 19:00:03 -0700

    Connect1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CastsService} from './services/casts.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontEnd';
+  constructor(private cast:CastsService) {
+    this.cast.getData().subscribe(data => {
+      console.warn(data)
+    })
+  }
 }
